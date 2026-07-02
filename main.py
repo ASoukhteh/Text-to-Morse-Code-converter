db7787e971e903f1845b62f8fdf7df7c47310794
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-02 20:29:24 +0330

    handle errors if input be wrong will raise an message that tell what should you actually your input be.

--- main.py
+++ main.py
@@ -10,13 +10,28 @@ while True:
 
     if operation == "e":
         break
+
     elif operation == 'encode':
         text = input("Enter your text here: \n")
-        code = morse_code.encode(text)
-        print("Your Morse Code is: {}".format(code), end='\n')
+        try:
+            code = morse_code.encode(text)
+            print("Your Morse Code is: {}".format(code), end='\n')
+        except KeyError:
+            print(text)
+            print("Your text must only contains alphabets and digit numbers.")
+
     elif operation == 'decode':
-        text = morse_code.decode()
-        print(text, end='\n')
+        try:
+            text = morse_code.decode()
+            print("Your message is: {}".format(text, end='\n'))
+        except KeyError:
+            print(
+                """
+                    Type morse code using '.', '-', 
+                    using a spaces between letters and
+                    '/' between words.
+                """
+            )
     else:
         print("OOPS! You have an typo error, make sure to write it right.")
     
