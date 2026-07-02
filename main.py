f3de8a7cc3730cae133b917b58398b5a57c06ae3
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-02 19:15:29 +0330

    know encode works

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from art import art
+from morse import MorseCode
+
+# operation = input("Do you wish to Encode or Decode? \n").lower()
+
+morse_code = MorseCode()
+
+print(morse_code.encode('sos'))
 
