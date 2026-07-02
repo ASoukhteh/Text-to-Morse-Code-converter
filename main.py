f2944c65902fe48ea68ef41b8af467df2fa23b07
AmirHSoukhteh <a.h.soukhteh@gmail.com>
2026-07-02 20:08:52 +0330

    now program working good

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from art import art
 from morse import MorseCode
 
-# operation = input("Do you wish to Encode or Decode? \n").lower()
+print(art)
 
 morse_code = MorseCode()
 
-print(morse_code.encode('sos help'))
+while True:
+    operation = input("Do you wish to Encode or Decode? (e for EXIT)\n").lower()
 
-print(morse_code.decode())
+    if operation == "e":
+        break
+    elif operation == 'encode':
+        text = input("Enter your text here: \n")
+        code = morse_code.encode(text)
+        print("Your Morse Code is: {}".format(code), end='\n')
+    elif operation == 'decode':
+        text = morse_code.decode()
+        print(text, end='\n')
+    else:
+        print("OOPS! You have an typo error, make sure to write it right.")
+    
+
+
 
